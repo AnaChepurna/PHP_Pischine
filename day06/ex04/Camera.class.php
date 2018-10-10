d10edd26136ca5a5796df72d8d7df3c79bed6e2e
AnaChepurna <ch.nastasie@gmail.com>
2018-10-10 23:15:29 +0300

    fg

--- day06/ex04/Camera.class.php
+++ day06/ex04/Camera.class.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once '../ex03/Matrix.class.php';
+
 class Camera {
   public static $verbose = false;
   private $_position;
@@ -9,7 +12,7 @@ class Camera {
   private $_matrix;
   private $_width;
   private $_height;
-  function __construct(array $tab) {
+  public function __construct(array $tab) {
     $this->_position = $tab["origin"];
     $this->_translate_matrix = new Matrix(array("preset" => Matrix::TRANSLATION, "vtc" => (new Vector(array("dest" => $this->_position)))->opposite()));
     $this->_rotate_matrix = $tab["orientation"];
@@ -33,34 +36,24 @@ class Camera {
       echo "Camera instance constructed\n";
     }
   }
-  function __destruct() {
+  
+  public function __destruct() {
     if (Camera::$verbose) {
       echo "Camera instance destructed\n";
     }
   }
-  function watchVertex(Vertex $vertex) {
+
+  public function watchVertex(Vertex $vertex) {
     $vertex = $this->_project_matrix->transformVertex($this->_translate_rotate_matrix->transformVertex($vertex));
     $vertex->__set("x", ($vertex->__get("x") / ($this->_width / 2)) - 1);
     $vertex->__set("y", ($vertex->__get("y") / ($this->_height / 2)) - 1);
     return ($vertex);
   }
-  function watchTriangle(Triangle $triangle) {
-    $v1 = $this->watchVertex($triangle->__get("v1"));
-    $v2 = $this->watchVertex($triangle->__get("v2"));
-    $v3 = $this->watchVertex($triangle->__get("v3"));
-    return (new Triangle(array("A" => $v1, "B" => $v2, "C" => $v3)));
-  }
-  function watchMesh(array $mesh) {
-    $new = array();
-    foreach ($mesh as $triangle) {
-      array_push($new, $this->watchTriangle($triangle));
-    }
-    return ($new);
-  }
-  function doc() {
+
+  public function doc() {
     return (file_get_contents("Camera.doc.txt"));
   }
-  function __toString() {
+  public function __toString() {
     $string = "Camera( \n";
     $string.= "+ Origine: ".$this->_position."\n";
     $string.= "+ tT:\n";
