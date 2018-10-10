3355012f01bd77b2ed369fa3a7b05a7f7cebbe16
AnaChepurna <ch.nastasie@gmail.com>
2018-10-10 17:13:32 +0300

    ff

--- day06/ex02/Vector.class.php
+++ day06/ex02/Vector.class.php
@@ -1,33 +1,32 @@
 <?php
 
-require_once '../ex00/Color.class.php';
+require_once '../ex01/Vertex.class.php';
 
-Class Vertex {
+Class Vector {
 	public static $verbose = False;
 	private $_x;
 	private $_y;
 	private $_z;
-	private $_w = 1;
-	private $_color;
+	private $_w = 0;;
 
 	public function __construct($arr)
 	{
 		//var_dump($arr);
-		$this->_x = $arr["x"];
-		$this->_y = $arr["y"];
-		$this->_z = $arr["z"];
-		if (isset($arr["w"]))
-			$this->_w = $arr["w"];
-		if (isset($arr["color"]) && $arr["color"] instanceof Color)
-		{
-			$this->_color = $arr["color"];
-			//var_dump($this->_color);
-			//var_dump($arr["color"]);
-		}
-		else
-			$this->_color = new Color(array("rgb" => 0xffffff));
-		if (self::$verbose)
-			print($this." constructed.\n");
+		// $this->_x = $arr["x"];
+		// $this->_y = $arr["y"];
+		// $this->_z = $arr["z"];
+		// if (isset($arr["w"]))
+		// 	$this->_w = $arr["w"];
+		// if (isset($arr["color"]) && $arr["color"] instanceof Color)
+		// {
+		// 	$this->_color = $arr["color"];
+		// 	//var_dump($this->_color);
+		// 	//var_dump($arr["color"]);
+		// }
+		// else
+		// 	$this->_color = new Color(array("rgb" => 0xffffff));
+		// if (self::$verbose)
+		// 	print($this." constructed.\n");
 	}
 
 	public function __destruct()
@@ -47,7 +46,7 @@ Class Vertex {
 
 	public static function doc()
    	{
-   		$fd = fopen("Vertex.doc.txt", 'r');
+   		$fd = fopen("Vector.doc.txt", 'r');
    		while ($fd && !feof($fd))
    			echo fgets($fd);
    		echo "\n";
